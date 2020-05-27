b3ae57263ec4c048c183a3d57af97ee2b6c1aa06
Diego Hurtado <ocelotl@users.noreply.github.com>
2020-05-26 19:36:18 -0600

    More WIP

--- ext/opentelemetry-ext-otlpexporter/src/opentelemetry/ext/otlpexporter/metrics_exporter/__init__.py
+++ ext/opentelemetry-ext-otlpexporter/src/opentelemetry/ext/otlpexporter/metrics_exporter/__init__.py
@@ -17,14 +17,18 @@
 import logging
 from time import sleep
 
+from google.protobuf.timestamp_pb2 import Timestamp
 from backoff import expo
 from grpc import StatusCode, insecure_channel, RpcError
 from typing import Sequence
 
-from opentelemetry.proto.collector.trace.v1.trace_service_pb2_grpc import (
-    TraceService
+from opentelemetry.proto.collector.metrics.v1.\
+        metrics_service_pb2_grpc import MetricsServiceStub
+from opentelemetry.proto.collector.metrics.v1.\
+        metrics_service_pb2 import ExportMetricsServiceRequest
+from opentelemetry.proto.metrics.v1.metrics_pb2 import (
+    Metric, MetricDescriptor
 )
-from opentelemetry.proto.metrics.v1.metrics_pb2 import Metric
 from opentelemetry.sdk.metrics.export import (
     MetricsExporter,
     MetricRecord,
@@ -40,7 +44,7 @@ class OTLPMetricsExporter(MetricsExporter):
 
     def __init__(self):
         super().__init__()
-        self._client = TraceService(insecure_channel(self.endpoint))
+        self._client = MetricsServiceStub(insecure_channel(self.endpoint))
 
     def export(
         self, metric_records: Sequence[MetricRecord]
@@ -88,11 +92,98 @@ class OTLPMetricsExporter(MetricsExporter):
 
             return MetricsExportResult.SUCESS
 
+    def generate_metrics_requests(
+        self, metrics: Sequence[MetricRecord]
+    ) -> ExportMetricsServiceRequest:
+        collector_metrics = translate_to_collector(metrics)
+        service_request = ExportMetricsServiceRequest(
+            node=self.node, metrics=collector_metrics
+        )
+        yield service_request
+
     def shutdown(self):
         pass
 
 
+# pylint: disable=too-many-branches
 def translate_to_collector(
     metric_records: Sequence[MetricRecord],
 ) -> Sequence[Metric]:
-    pass
+    collector_metrics = []
+    for metric_record in metric_records:
+
+        label_values = []
+        label_keys = []
+        for label_tuple in metric_record.labels:
+            label_keys.append(LabelKey(key=label_tuple[0]))
+            label_values.append(
+                LabelValue(
+                    has_value=label_tuple[1] is not None, value=label_tuple[1]
+                )
+            )
+
+        metric_descriptor = MetricDescriptor(
+            name=metric_record.metric.name,
+            description=metric_record.metric.description,
+            unit=metric_record.metric.unit,
+            type=get_collector_metric_type(metric_record.metric),
+            label_keys=label_keys,
+        )
+
+        timeseries = TimeSeries(
+            label_values=label_values,
+            points=[get_collector_point(metric_record)],
+        )
+        collector_metrics.append(
+            Metric(
+                metric_descriptor=metric_descriptor, timeseries=[timeseries]
+            )
+        )
+    return collector_metrics
+
+
+# pylint: disable=no-else-return
+def get_collector_metric_type(metric: Metric) -> MetricDescriptor:
+    if isinstance(metric, Counter):
+        if metric.value_type == int:
+            return MetricDescriptor.CUMULATIVE_INT64
+        elif metric.value_type == float:
+            return MetricDescriptor.CUMULATIVE_DOUBLE
+    return MetricDescriptor.UNSPECIFIED
+
+
+def get_collector_point(metric_record: MetricRecord) -> Point:
+    # TODO: horrible hack to get original list of keys to then get the bound
+    # instrument
+    point = Point(
+        timestamp=proto_timestamp_from_time_ns(
+            metric_record.aggregator.last_update_timestamp
+        )
+    )
+    if metric_record.metric.value_type == int:
+        point.int64_value = metric_record.aggregator.checkpoint
+    elif metric_record.metric.value_type == float:
+        point.double_value = metric_record.aggregator.checkpoint
+    else:
+        raise TypeError(
+            "Unsupported metric type: {}".format(
+                metric_record.metric.value_type
+            )
+        )
+    return point
+
+
+def proto_timestamp_from_time_ns(time_ns):
+    """Converts datetime to protobuf timestamp.
+
+    Args:
+        time_ns: Time in nanoseconds
+
+    Returns:
+        Returns protobuf timestamp.
+    """
+    ts = Timestamp()
+    if time_ns is not None:
+        # pylint: disable=no-member
+        ts.FromNanoseconds(time_ns)
+    return ts
