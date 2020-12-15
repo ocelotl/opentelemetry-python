ceac02c9eb834a57b1e61e308c78e0e829cc10f3
github-action-benchmark <github@users.noreply.github.com>
2020-12-15 18:08:24 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - core (pytest) benchmark result for 001163739d7cc09a26592b688f9880da02baf208

---
+++ benchmarks/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1608055704432,
+  "repoUrl": "https://github.com/ocelotl/opentelemetry-python",
+  "entries": {
+    "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "001163739d7cc09a26592b688f9880da02baf208",
+          "message": "Remove SDK dependency from auto-instrumentation (#1420)",
+          "timestamp": "2020-12-14T15:03:25-08:00",
+          "tree_id": "3a2c11fff71c03c32ec35d12b2a7e3336dd0c7f9",
+          "url": "https://github.com/ocelotl/opentelemetry-python/commit/001163739d7cc09a26592b688f9880da02baf208"
+        },
+        "date": 1608055702728,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 27318.15144909692,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000012599218965716674",
+            "extra": "mean: 36.60569793177048 usec\nrounds: 4499"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 20358.69630876818,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000002432099151059304",
+            "extra": "mean: 49.11905874686658 usec\nrounds: 6911"
+          }
+        ]
+      }
+    ]
+  }
+}
