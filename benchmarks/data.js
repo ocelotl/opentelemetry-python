15cfdc45826438ab0ff6b46b05b69f1b2b5eb4aa
github-action-benchmark <github@users.noreply.github.com>
2021-01-28 16:53:21 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - exporter (pytest) benchmark result for c026c0f6a53adae1af6cf36fb4bb830a31c6ffc9

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611852777102,
+  "lastUpdate": 1611852801806,
   "repoUrl": "https://github.com/ocelotl/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -2560,6 +2560,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0007776006649717735",
             "extra": "mean: 193.95629790798685 usec\nrounds: 6549"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ewan.higgs@gmail.com",
+            "name": "Ewan Higgs",
+            "username": "ehiggs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c026c0f6a53adae1af6cf36fb4bb830a31c6ffc9",
+          "message": "1542. Allow missing carrier headers to continue without raising AttributeError (#1545)",
+          "timestamp": "2021-01-27T08:31:32-08:00",
+          "tree_id": "cd2a50fa811d6310e1d6bf9b75512412d2a30766",
+          "url": "https://github.com/ocelotl/opentelemetry-python/commit/c026c0f6a53adae1af6cf36fb4bb830a31c6ffc9"
+        },
+        "date": 1611852800283,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 2516.069816482598,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0003120278113081479",
+            "extra": "mean: 397.4452511011697 usec\nrounds: 227"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 3986.933999976523,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008528236686705778",
+            "extra": "mean: 250.81930124900197 usec\nrounds: 5444"
           }
         ]
       }
